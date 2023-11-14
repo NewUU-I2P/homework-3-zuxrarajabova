6d4263b38e208f5a353bcfdafb83b78143e5b773
z.rajabova <z.rajabova@newuu.uz>
2023-11-14 11:47:28 +0500

    hw3

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 
 float problemSolution1(float consumed_water) {
-    float cost;
+    float cost, fixed = 13;
     // write your code here
 
-    if (consumed_water > 60) {
-        cost += (consumed_water - 60) * 1.5; //additional
-    } else if (consumed_water > 50) {
-        cost += (consumed_water - 50) * 1.4; //10
-    } else if (consumed_water > 30) {
-        cost += (consumed_water - 30) * 0.12; //20
-    } else {
-        cost += consumed_water * 0.4; //30
+    if (consumed_water <= 30) {
+        cost = fixed + (consumed_water * 0.4);
+    } else if (consumed_water > 30 && consumed_water <= 50) {
+        cost = fixed + ((consumed_water - 30) * 0.12) + (30 * 0.4);
+    } else if (consumed_water > 50 && consumed_water <= 60) {
+        cost = fixed + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
+    } else if (consumed_water > 60) {
+        cost = fixed + ((consumed_water - 60) * 1.5) + (30 * 0.4) + (20 * 0.12) + (10 * 1.4);
     }
-
-    cost += 13;
 
     return cost;
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -4,13 +4,9 @@
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
 
-    if (macAddress.size() != 17) {
-        return "invalid";
-    }
-
     unsigned firstOctet = std::stoul(macAddress.substr(0, 2), nullptr, 16);
 
-    if (firstOctet = 0xFF) {
+    if (firstOctet == 255) {
         return "Broadcast";
     } else if (firstOctet % 2 == 0) {
         return "Unicast";
